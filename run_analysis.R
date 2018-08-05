7613e4f01295b822f0f7e136ddb30e5b68ec3acf
kanzq8 <kanzq8@hotmail.com>
2018-08-06 00:56:49 +0300

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -1,12 +1,12 @@
-my_train_set <- read.csv("C:\\Coursera\\Cleaning\\HAR Dataset\\UCI HAR Dataset\\train\\X_train.txt", sep= "", header = FALSE)
-my_train_act <- read.csv("C:\\Coursera\\Cleaning\\HAR Dataset\\UCI HAR Dataset\\train\\y_train.txt", sep= "", header = FALSE, col.names = "V562")
-my_train_ref <- read.csv("C:\\Coursera\\Cleaning\\HAR Dataset\\UCI HAR Dataset\\train\\subject_train.txt", sep= "", header = FALSE, col.names = "V563")
+my_train_set <- read.csv("train\\X_train.txt", sep= "", header = FALSE)
+my_train_act <- read.csv("train\\y_train.txt", sep= "", header = FALSE, col.names = "V562")
+my_train_ref <- read.csv("train\\subject_train.txt", sep= "", header = FALSE, col.names = "V563")
 
 my_train_set <- cbind(my_train_set, my_train_act, my_train_ref)
 
-my_test_set <- read.csv("C:\\Coursera\\Cleaning\\HAR Dataset\\UCI HAR Dataset\\test\\X_test.txt", sep= "", header = FALSE)
-my_test_act <- read.csv("C:\\Coursera\\Cleaning\\HAR Dataset\\UCI HAR Dataset\\test\\y_test.txt", sep= "", header = FALSE, col.names = "V562")
-my_test_ref <- read.csv("C:\\Coursera\\Cleaning\\HAR Dataset\\UCI HAR Dataset\\test\\subject_test.txt", sep= "", header = FALSE, col.names = "V563")
+my_test_set <- read.csv("test\\X_test.txt", sep= "", header = FALSE)
+my_test_act <- read.csv("test\\y_test.txt", sep= "", header = FALSE, col.names = "V562")
+my_test_ref <- read.csv("test\\subject_test.txt", sep= "", header = FALSE, col.names = "V563")
 
 my_test_set <- cbind(my_test_set, my_test_act, my_test_ref)
 
@@ -14,7 +14,7 @@ my_data_set <- rbind(my_train_set, my_test_set)
 
 library(dplyr)
 
-my_features <- read.csv("C:\\Coursera\\Cleaning\\HAR Dataset\\UCI HAR Dataset\\features.txt", sep = "", header = FALSE)
+my_features <- read.csv("features.txt", sep = "", header = FALSE)
 
 my_features <- rbind(my_features, cbind('562', 'Activity'), cbind('563', 'Subject'))
 
@@ -27,3 +27,9 @@ names(my_data_set) <- my_features$V2
 my_subset <- my_data_set[c(as.integer(my_columns$V1))]
 
 my_agg <- aggregate(my_subset[,1:66], by=list(my_subset$Activity, my_subset$Subject), FUN = mean)
+
+write.csv(my_subset, file="subset.csv")
+
+write.csv(my_agg, file ="aggregate.csv")
+
+write.table(my_agg, file = "aggregate.txt", row.names = FALSE)
